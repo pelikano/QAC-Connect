3f0ec60a3683e0c6538ab1c2b885d00d95b2123b
Fabio Pezzutto <fvp@qad.com>
2015-10-09 14:57:14 +0000

    Changes from Screen Builder

---
+++ QAD Connect TEST/dataProviders/anacapa.js
@@ -0,0 +1,11 @@
+'use strict';
+
+(function() {
+    app.data.anacapa = {
+        url: 'http://coli22:23000/qad-acmeui/api/qracore/inboxdata?loadAll=true&domainCode=10USA&entityCode=10CORPCONS'
+    };
+
+}());
+
+// START_CUSTOM_CODE_anacapa
+// END_CUSTOM_CODE_anacapa
